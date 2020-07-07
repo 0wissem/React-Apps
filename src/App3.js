6b9134e7831b97aa20088e663af581bd127d731e
0wissem <48405008+0wissem@users.noreply.github.com>
2020-07-07 22:00:12 +0100

    Update App3.js

--- src/App3.js
+++ src/App3.js
@@ -10,9 +10,9 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-          <Display name= "Mannoubia" photo= {photo1}  desc='chedda dar' gender='female'></Display>
-          <Display name= "Ftou7" photo= {photo2}  desc='yrakka7 fel bnet b le7ytou' gender='male'></Display>
-          <Display name= "Lamis" photo= {photo3}  desc='saybet ftou7 tla3 mnattef' gender='female'></Display>
+          <Display name= "Mannoubia" photo= {photo1}  desc='cfdkjn' gender='female'></Display>
+          <Display name= "Ftou7" photo= {photo2}  desc='fejhdfj' gender='male'></Display>
+          <Display name= "Lamis" photo= {photo3}  desc='fkejdskfj' gender='female'></Display>
       </div>
     );
   }
